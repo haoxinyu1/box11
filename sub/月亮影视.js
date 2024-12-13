30f95ecd0aae09417a3c6b70744dace9e2e07450
hogo18 <113156723+hogo18@users.noreply.github.com>
2024-12-13 14:19:29 +0800

    Update 月亮影视.js

--- sub/月亮影视.js
+++ sub/月亮影视.js
@@ -1,11 +1,47 @@
 var rule = {
 	title: '月亮影视',
-	host: 'https://www.woaimoon.net/',
-        url:'/vodshow/fyfilter.html',
-	//url: '/vodshow/fyclass/page/fypage.html',
-	filterable: 1,//是否启用分类筛选,
-	play_parse:true,
-	filter_url: '{{fl.cateId}}/page/fypage/year/{{fl.year}}',
+	模板:'mxpro',
+	host: 'https://tv.woaimoon.net/',
+	url:'/vodshow/fyclass/fyfilter.html',///vodshow/1/class/{{fl.class}}/page/3/year/2023.html
+	filterable: 1,
+	filter_url: '/page/fypage/year/{{fl.year}}',
+	searchUrl: '/vodsearch.html?wd=**',///vodsearch.html?wd=**
+	searchable: 2,
+	class_name: '电视剧&电影&动漫&综艺&短剧',
+	class_url: '2&1&3&4&19',
+	headers: {
+		'User-Agent': 'PC_UA',
+	},
+	play_parse: true,
+	lazy: `js:
+		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+		var url = html.url;
+		if (html.encrypt == '1') {
+			url = unescape(url)
+		} else if (html.encrypt == '2') {
+			url = unescape(base64Decode(url))
+		}
+		if (/\\.m3u8|\\.mp4/.test(url)) {
+			input = {
+				jx: 0,
+				url: url,
+				parse: 0
+			}
+		} else {
+			input
+		}
+	`,
+	//推荐: '.vodlist;*;*;*;*;*',
+	一级: '.vodlist div.pack-ykpack;.vodname&&Text;.eclazy&&data-original;.pack-prb&&Text;a&&href',
+	二级: {
+		"title": ".fyy&&Text;.s-top-info-detail&&Text",
+		"img": ".g-playicon&&img&&src",
+		"desc": ".s-top-info-title span&&Text;;;.item-type&&Text;.item-actor:eq(2)&&Text",
+		"content": ".ec-palytcji span&&Text",
+		"tabs": ".channelname",
+		"lists": ".content_playlist:eq(#id) li"
+	},
+	搜索: '.search-list;a&&title;*;*;*',
 	filter: 
 	{
         "1": [ {"key": "year","name": "年份","value": 
@@ -49,47 +85,4 @@ var rule = {
 			    }
 	         ]
     },
-	filter_def: {
-		1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'}
-	},
-	searchUrl: '/vodsearch/page/fypage/wd/**.html',
-	searchable: 2,//是否启用全局搜索,
-	headers: {//网站的请求头,完整支持所有的,常带ua和cookies
-		'User-Agent': 'PC_UA',
-	},
-	class_parse: '.nav-channel a;a&&Text;a&&href;/(\\d+).html',
-	lazy: `js:
-		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-		var url = html.url;
-		if (html.encrypt == '1') {
-			url = unescape(url)
-		} else if (html.encrypt == '2') {
-			url = unescape(base64Decode(url))
-		}
-		if (/\\.m3u8|\\.mp4/.test(url)) {
-			input = {
-				jx: 0,
-				url: url,
-				parse: 0
-			}
-		}else {
-			input
-		}
-	`,
-	limit: 6,
-	double: true, // 推荐内容是否双层定位
-	推荐: '.vodlist;*;*;*;*;*',
-	一级: '.pack-ykpack;a&&title;.eclazy&&data-original;.pack-prb&&Text;a&&href',
-	二级: {
-		"title": ".fyy&&Text;.s-top-info-detail&&Text",
-		"img": ".g-playicon&&img&&src",
-		"desc": ".s-top-info-title span&&Text;;;.item-type&&Text;.item-actor:eq(2)&&Text",
-		"content": ".ec-palytcji span&&Text",
-		"tabs": ".swiper-wrapper&&a",
-		"lists": ".content_playlist:eq(#id) li"
-	},
-	搜索: '.pack-packcover.returl.list-top-b;*;*;*;*',
 }
