72b1359c794f01b2538fafe106faa9f3a2398531
hogo18 <113156723+hogo18@users.noreply.github.com>
2026-08-10 14:44:08 +0800

    Update yb.js

--- cat/yb.js
+++ cat/yb.js
@@ -26,8 +26,8 @@ async function init(cfg) {
 
 async function home(filter) {
     const classes = [
-        { type_id: "1", type_name: "电影" },
         { type_id: "2", type_name: "剧集" },
+        { type_id: "1", type_name: "电影" },
         { type_id: "3", type_name: "动漫" },
         { type_id: "4", type_name: "综艺" }
     ];
